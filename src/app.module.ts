b5abe5059a6bd8482930c2955854bc118c1d359e
william nascimentom <williamnnmiranda@gmail.com>
2023-11-15 14:46:40 -0300

    feat: :sparkles: created template crud product

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,13 @@ import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
 import { userRepositoryImpl } from './repository/user/userRepositoryImpl';
 import { CartModule } from './cart/cart.module';
+import { ProductModule } from './product/product.module';
 @Module({
   imports: [UserModule, AuthModule, JwtModule.register({
     global: true,
     secret: 'jwtConstants.secret',
     signOptions: { expiresIn: '60s' },
-  }), CartModule],
+  }), CartModule, ProductModule],
   controllers: [],
   providers: [PrismaService, AuthService, userRepositoryImpl],
 })
